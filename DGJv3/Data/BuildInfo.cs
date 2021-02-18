d6ffca0397ab47fe8f80a2901f8495fe46746f4e
CoelWu <coelwu78@protonmail.com>
2021-02-18 21:48:35 +0800

    [Version Number] Change Version To 3.1.0

--- DGJv3/Data/BuildInfo.cs
+++ DGJv3/Data/BuildInfo.cs
@@ -3,7 +3,7 @@
     internal class BuildInfo
     {
         internal const bool Appveyor = false;
-        internal const string Version = @"1.0.3";
+        internal const string Version = @"3.1.0";
         internal const string HeadSha1 = @"3f4b62a3b286ab045e80d29944cf896d7d29f731";
         internal const string HeadShaShort = @"3f4b62a3";
     }
